53f0bd884f5d309f148525926dddb24460e2fa07
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-05-22 16:31:05 +0200

    created program_id method and twig

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramController extends AbstractController
@@ -13,6 +15,18 @@ class ProgramController extends AbstractController
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+    #[Route('/program/{id}', requirements: ['page' => '\d+'], methods: ['GET'], name: 'program_id')]
+    public function selectOneById($id = 1): Response
+    {
+
+        if (!is_numeric($id)) {
+            throw new NotFoundHttpException('404 - Page not found');
+        }
+        return $this->render('program/program_id.html.twig', [
+            'program_id' => 'Série n°' . $id,
         ]);
     }
 }
