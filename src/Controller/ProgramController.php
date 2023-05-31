c32bf24e0cb2fe2bc3bb6cd81a0cff7916ea8115
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-05-31 10:55:17 +0200

    added episode section and page with description

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,14 +2,13 @@
 
 namespace App\Controller;
 
-use App\Repository\SeasonRepository;
-use App\Repository\EpisodeRepository;
+use App\Entity\Season;
+use App\Entity\Episode;
+use App\Entity\Program;
 use App\Repository\ProgramRepository;
-use Doctrine\Common\Collections\Collection;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
@@ -26,43 +25,41 @@ class ProgramController extends AbstractController
 
 
     #[Route('/show/{id<^[0-9]+$>}', methods: ['GET'], name: 'show')]
-    public function show(int $id,
-    ProgramRepository $programRepository,
-    SeasonRepository $seasonRepository,
-    EpisodeRepository $episodeRepository,
-    ): Response
-    {
-        $program = $programRepository->findOneBy(['id' => $id]);
-        $seasons = $seasonRepository->findBy(['program' => $program]);
-        $episodes = $episodeRepository->findBy(['season' => $seasons]);
-
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'No program with id : ' . $id . ' found in program\'s table.'
-            );
-        }
+    public function show(
+        Program $program,
+    ): Response {
 
         return $this->render('program/show.html.twig', [
             'program' => $program,
-            'seasons' => $seasons,
-            'episodes' => $episodes,
         ]);
     }
 
-    #[Route('/program/{programId}/seasons/{seasonId}', name: 'season_show')]
+    #[Route('/program/{program}/season/{season}', name: 'season_show')]
     public function showSeason(
-        int $programId,
-        int $seasonId,
-        ProgramRepository $programRepository,
-        SeasonRepository $seasonRepository,
-        ): Response
-    {
-        $program = $programRepository->findOneBy(['id' => $programId]);
-        $season = $seasonRepository->findOneBy(['id' => $seasonId]);
+        Program $program,
+        Season $season,
+    ): Response {
+
+        $episodes = $season->getEpisodes();
 
         return $this->render('program/season_show.html.twig', [
             'program' => $program,
             'season' => $season,
+            'episodes' => $episodes,
+        ]);
+    }
+
+    #[Route('program/{program}/season/{season}/episode/{episode}', name: 'episode_show')]
+    public function showEpisode(
+        Program $program,
+        Season $season,
+        Episode $episode
+    ): Response {
+
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episode' => $episode,
         ]);
     }
 }
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\ProgramRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ProgramRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: ProgramRepository::class)]
 class Program
@@ -26,6 +28,13 @@ class Program
     #[ORM\ManyToOne(inversedBy: 'programs')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
+
+    #[ORM\OneToMany(mappedBy: 'program', targetEntity: Season::class)]
+    private Collection $seasons;
+
+    public function __construct() {
+        $this->seasons = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -77,6 +86,37 @@ class Program
     {
         $this->category = $category;
 
+        return $this;
+    }
+
+    public function getSeasons(): Collection
+    {
+        return $this->seasons;
+    }
+
+    public function setSeasons(Collection $seasons): self
+    {
+        $this->seasons = $seasons;
+
+        return $this;
+    }
+
+    public function addSeason(Season $season): self
+    {
+        if (!$this->seasons->contains($season)) {
+            $this->seasons->add($season);
+            $season->setProgram($this);
+        }
+        return $this;
+    }
+    public function removeSeason(Season $season): self
+    {
+        if ($this->seasons->removeElement($season)) {
+            // set the owning side to null (unless already changed)
+            if ($season->getProgram() === $this) {
+                $season->setProgram(null);
+            }
+        }
         return $this;
     }
 }
